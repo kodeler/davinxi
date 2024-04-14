339801b7fa779757741068d5363e321bef564c58
rosinver.vz <rosinver.vz@gmail.com>
2024-04-14 23:28:46 +0000

    commit 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 'use client'
+import React from 'react';
 import Spline from '@splinetool/react-spline';
-export default function App() {
+
+export default function SplineComponent() {
   return (
-    <Spline scene="https://draft.spline.design/9lMrBnhabDli1fkB/scene.splinecode" />
+    <div className="flex justify-center items-center h-screen">
+      <Spline scene="https://draft.spline.design/9lMrBnhabDli1fkB/scene.splinecode" />
+    </div>
   );
 }
