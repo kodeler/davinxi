dffd8eba066e1352959e8dfadfcfe141bddbc0ed
rosinver.vz <rosinver.vz@gmail.com>
2024-04-14 22:53:41 +0000

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+export default function App() {
+  return (
+    <Spline scene="https://draft.spline.design/9lMrBnhabDli1fkB/scene.splinecode" />
+  );
+}
